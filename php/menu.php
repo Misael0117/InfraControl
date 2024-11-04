e0cae38776186e730985da6979be096ff54f0c96
Misael Alejandro Tucuch Bonilla <misaelalekandrot@gmail.com>
2024-11-04 11:08:20 -0600

    Se creo la carpeta para contener los archivos php

---
+++ php/menu.php
@@ -0,0 +1,22 @@
+<!-- Menu lateral -->
+
+<nav id="sidebarMenu" class="col-md-2 d-none d-md-block bg-light sidebar">
+    <div class="sidebar-sticky">
+        <h4 class="pt-3">Menú</h4>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="home.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="consultas.php">Consultas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Reportes</a>
+            </li>
+        <li>
+        <a href="index.php" class="btn btn-primary">Cerrar Sesión</a>
+        </li>
+
+        </ul>
+    </div>
+</nav>
